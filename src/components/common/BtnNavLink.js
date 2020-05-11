4288301eb9d0c6c7ebbc11c8a4c157c0619d43d8
julioalcantara <alcantara.juliosousa@gmail.com>
2020-05-11 21:39:48 +0100

    nav links

--- src/components/common/BtnNavLink.js
+++ src/components/common/BtnNavLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 import Spacer from './Spacer';
 
-const NavLink = ({ text }) => {
+const BtnNavLink = ({ text }) => {
     return (
         <Spacer>
             <Text style={styles.link}>{text}</Text>
@@ -13,8 +13,8 @@ const NavLink = ({ text }) => {
 const styles = StyleSheet.create({
     link: {
         color: 'blue',
-        fontSize: 15
+        fontSize: 20
     }
 });
 
-export {NavLink};
+export {BtnNavLink};
