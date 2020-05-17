3144f7e48886a926af6fd45a2493417b6cfa5e29
julioalcantara <alcantara.juliosousa@gmail.com>
2020-05-17 13:50:29 +0100

    fix navigation

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,17 +1,13 @@
 import { 
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
-    LOGIN_USER_SUCESS,
     LOGIN_USER_FAIL,
-    LOGIN_USER,
     CREATE_ACCOUNT,
     CREATE_ACC_SUCCESS,
     LOGIN_ADMIN,
     CREATE_ADMIN,
     LOGIN_ADMIN_SUCCESS,
     CREATE_ADMIN_SUCCESS,
-    LOGOUT_USER,
-    LOGOUT_USER_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = { 
@@ -29,16 +25,12 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED: 
             return { ...state, password: action.payload };
-        case LOGIN_USER:
-            return {...state, loading: true, error: ''}
         case LOGIN_ADMIN:
             return {...state, loading: true, error: ''}
         case CREATE_ACCOUNT:
             return {...state, loading: true, error: ''}
         case CREATE_ADMIN:
             return {...state, loading: true, error: ''}
-        case LOGIN_USER_SUCESS: 
-            return { ...state, ...INITIAL_STATE, user: action.payload, error: '', loading: false };
         case LOGIN_ADMIN_SUCCESS: 
             return { ...state, ...INITIAL_STATE, admin: action.payload, error: '', loading: false }; 
         case CREATE_ACC_SUCCESS: 
@@ -47,10 +39,6 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, ...INITIAL_STATE, admin: action.payload, error: '', loading: false };
         case LOGIN_USER_FAIL: 
             return { ...state, error: 'Authentication Failed.', password: '', loading: false };
-        case LOGOUT_USER:
-            return state;
-        case LOGOUT_USER_SUCCESS: 
-            return { INITIAL_STATE, error: '', loading: false };
         default:
             return state;
     }
